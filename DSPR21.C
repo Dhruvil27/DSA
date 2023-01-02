57a852c9b07babb7de1f773d1b94f23b30857204
Dhruvil27 <103164455+Dhruvil27@users.noreply.github.com>
2023-01-02 13:14:13 +0600

    Add files via upload

---
+++ DSPR21.C
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void main()
+{
+int a[5],pass,i,temp,size=5;
+clrscr();
+for(i=0;i<size;i++)
+{
+printf("Enter Element:");
+scanf("%d",&a[i]);
+}
+
+for(pass=0;pass<size-1;pass++)
+{
+for(i=0;i<size-pass-1;i++)
+{
+if(a[i]>a[i+1])
+{
+temp=a[i];
+a[i]=a[i+1];
+a[i+1]=temp;
+}
+}
+
+printf("\n Output Of %dpass:",pass);
+for(i=0;i<size;i++)
+{
+printf("%d",a[i]);
+}
+}
+
+printf("\n sorted Array:");
+for(i=0;i<size;i++)
+{
+printf("%d",a[i]);
+}
+getch();
+}
+
+
